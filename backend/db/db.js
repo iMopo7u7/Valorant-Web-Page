0d1a8a8e3e2a0dd784660fb0349a65c4dc85a076
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2025-08-20 04:26:18 -0600

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,1 +1,26 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
+let db, playersCollection, matchesCollection;
+
+export async function connectDB() {
+  if (!process.env.MONGODB_URI) {
+    console.error("❌ ERROR: MONGODB_URI no está definido.");
+    process.exit(1);
+  }
+
+  try {
+    const client = new MongoClient(process.env.MONGODB_URI);
+    await client.connect();
+    db = client.db("valorantDB");
+    playersCollection = db.collection("players");
+    matchesCollection = db.collection("matches");
+    console.log("✅ Conectado a MongoDB");
+  } catch (err) {
+    console.error("❌ Error conectando a MongoDB:", err);
+    process.exit(1);
+  }
+}
+
+export { db, playersCollection, matchesCollection };
