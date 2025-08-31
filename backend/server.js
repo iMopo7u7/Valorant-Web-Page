2e2f8c9931ef102747a4dcb41f6ab0086b5d30a3
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2025-08-31 06:21:14 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,8 +58,8 @@ app.use(session({
   store: sessionStore,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: "none",
     maxAge: 60 * 60 * 1000
   }
 }));
